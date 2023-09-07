c13375a8ae23f0d690560faefe41be3348229f8a
zzangbae <bestisland01@gmail.com>
2023-09-07 23:46:28 +0900

    novice_mid-CallByValue,Reference: 기본swap문제풀이

---
+++ 1_novice_mid/1_함수/3_CallByValue&CallByReference/Swap.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
+public class Swap {
+    public static void swap(IntWrapper n, IntWrapper m) {
+        int temp = n.value;
+        n.value = m.value;
+        m.value = temp;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        IntWrapper n = new IntWrapper(sc.nextInt());
+        IntWrapper m = new IntWrapper(sc.nextInt());
+
+        swap(n, m);
+
+        System.out.println(n.value + " " + m.value);
+    }
+}
