6a3aa043f62d85c63355b089401872510b3ce277
zzangbae <bestisland01@gmail.com>
2023-10-30 23:16:59 +0900

    NL-출력-변수와자료형: 자료형에 맞춰 변수를 선언하고 해당 변수를 출력하는 연습을 함

---
+++ codetree/0_novice_low/1_출력/2_변수와자료형/Problem1.java
@@ -0,0 +1,8 @@
+// 변수와 자료형 - 정수 선언하고 뺄셈
+public class Problem1 {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 13;
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+}
---
+++ codetree/0_novice_low/1_출력/2_변수와자료형/Problem2.java
@@ -0,0 +1,9 @@
+// 변수 선언하기
+public class Problem2 {
+    public static void main(String[] args) {
+        int a = 3;
+        char c = 'C';
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
