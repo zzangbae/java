6fcfa7f25c1ed291e7202d94333fd3de363987d0
zzangbae <bestisland01@gmail.com>
2023-10-30 23:01:23 +0900

    NL-출력-변수값을동시에복사: 문제 수정 및 문제풀이 완료

--- codetree/0_novice_low/1_출력/8_변수값동시에복사/Problem3.java
+++ codetree/0_novice_low/1_출력/8_변수값동시에복사/Problem3.java
@@ -1,8 +1,8 @@
-// 정수 선언하고 곱 출력
+// 합을 복사하기
 public class Problem3 {
     public static void main(String[] args) {
-        int a = 26;
-        int b = 5;
-        System.out.println(a + " * " + b + " = " + a * b);
+        int a = 1; int b = 2; int c = 3;
+        a = b = c = a + b + c;
+        System.out.println(a + " " + b + " " + c);
     }
 }
