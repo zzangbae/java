45f52e4021c020ee24b5c15ea1bd59e7730698aa
zzangbae <bestisland01@gmail.com>
2023-11-10 17:05:46 +0900

    NL-단순반복문-for문a->b 1씩감소: 조건식을 컨트롤 --

--- codetree/0_novice_low/5_단순반복문/3_for문a->b_1씩감소/Problem1.java
+++ codetree/0_novice_low/5_단순반복문/3_for문a->b_1씩감소/Problem1.java
@@ -2,6 +2,6 @@
 public class Problem1 {
     public static void main(String[] args) {
         for(int i = 17; i >= 5; i--)
-            System.out.print(i + " ");
+            System.out.print(i + " "); // 17 16 ... 5
     }
 }
--- codetree/0_novice_low/5_단순반복문/3_for문a->b_1씩감소/Problem4.java
+++ codetree/0_novice_low/5_단순반복문/3_for문a->b_1씩감소/Problem4.java
@@ -1,3 +1,12 @@
+// 홀수만 출력2
+import java.util.Scanner;
+
 public class Problem4 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+        for(int i = b; i >= a; i -= 2)
+            System.out.print(i + " ");
+    }
 }
